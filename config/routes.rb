7ebc26b471ac18ffe3e3beb1d1d1e1f297b55aff
Nicholas Hammond <hammondn649@gmail.com>
2021-08-12 17:45:34 +1000

    add back routes for comments and notes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,18 @@ Rails.application.routes.draw do
     
     get '/pin_families/:id', to: 'pin_families#show'
 
+    get '/notes', to: 'notes#index'
+    post '/notes', to: 'notes#create'
+    get '/notes/:id', to: 'notes#show'
+    put '/notes/:id', to: 'notes#update'
+    delete '/notes/:id', to: 'notes#destroy'    
+
+    get '/comments', to: 'comments#index'
+    post '/comments', to: 'comments#create'
+    get '/comments/:id', to: 'comments#show'
+    put '/comments/:id', to: 'comments#update'
+    delete '/comments/:id', to: 'comments#destroy'
+
     resources :notes  
     resources :comments
   end
